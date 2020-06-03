d550fde2c14c79a689e10e4cefa4f0ffdee06ae3
Stephen Bowen <sfbowen4@gmail.com>
2020-06-03 15:29:50 -0500

    sdf

--- Scraper.py
+++ Scraper.py
@@ -1,75 +1,48 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup as b
 import time
-import login
-import getpages
+import urllib
+import requests
+from splinter import Browser
 
-username = 'cordlove.organizer'
+
+username = 'bigchungy42069'
 password = 'ClearlyInventory2020!'
-driver = 0
-refs = []
-max_likes = 350
-max_follows = 50
+hashtag = 'trucks'
+hashtagpage = 'https://www.instagram.com/explore/tags/{}'.format(hashtag)
 
-def main():
-	global driver
-	print('running script..')
-	driver = webdriver.Chrome('C://Users/Ryan/Desktop/chromedriver.exe')
-	l = login.Login(driver, username, password)
-	l.signin()
-	gp = getpages.Getpages(driver)
-	refs = gp.get_followers()
-	print(gp.get_num_flw())
-	run_bot(refs, driver, gp)
-	
-def run_bot(refs, driver, gp):
-	print(len(refs))
-	print('accounts targeted')
-	t = time.time()
-	#how many pages we likes / followed
-	L = 0
-	F = 0
-	for r in refs:
-		driver.get('https://www.instagram.com' + r)
-		time.sleep(2)
-		if gp.get_num_flw() < 3000:
-			if gp.is_public():
-				print('public account')
-				print('current likes: ' + str(L))
-				if L < max_likes:
-					try:
-						gp.like_post()
-						L += 1
-						print("POST LIKED")
-					except:
-						print('could not like..lets follow instead')
-						try:
-							gp.follow_page()
-							print('page followed successfully')
-							F += 1
-						except:
-							print('could not follow')
-				else:
-					time.sleep(3600) # time.sleep(3600 - (time.time() - t)) -> t= time.time()
-			else:
-				print('account is private')
-				print('current follows: ' + str(F))
-				if F < max_follows:
-					time.sleep(2)
-					try:
-						gp.follow_page()
-						print('page followed successfully')
-						F += 1
-					except:
-						print('could not follow')
-					
-				else:
-					time.sleep(3600)
+#Browser stuff
+executable_path = {'executable_path': r'/Users/stephen/Desktop/chromedriver'}
+browser = Browser('chrome', **executable_path, headless = False)
 
+#URL components
+method = 'GET'
+url = 'https://www.instagram.com/accounts/login/?source=auth_switcher'
 
+browser.visit(url)
 
-if __name__ == '__main__':
-	main()	
+#define elements to pass through the form
+browser.find_by_name("username").first.fill(username)
+browser.find_by_name("password").first.fill(password)
+browser.find_by_text("Log In").first.click()
+time.sleep(2)
+
+#save info
+browser.find_by_text("Not Now").first.click()
+time.sleep(2)
+#turn on notifications
+browser.find_by_text("Not Now").first.click()
+time.sleep(2)
+#go to hashtag page
+browser.visit(hashtagpage)
+
+links = []
+tags = browser.links.find_by_partial_href('/p/')
+for x in tags:
+    links.append(x['href'])
+
+for x in links:
+    browser.visit(x)
+    try:
+        browser.find_by_text('Follow').first.click()
+    except:
+        print('Already Followed')
+    time.sleep(5)
