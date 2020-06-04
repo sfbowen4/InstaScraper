b210d9e48137c6aac8b54112f577b03b2eb7d3dd
Stephen Bowen <sfbowen4@gmail.com>
2020-06-03 21:58:50 -0500

    asdf

--- Scraper.py
+++ Scraper.py
@@ -3,10 +3,18 @@ import urllib
 import requests
 import splinter
 
-username = 'bigchungy42069'
-password = 'ClearlyInventory2020!'
-hashtag = 'trucks'
+#username = 'bigchungy42069'
+#password = 'ClearlyInventory2020!'
+
+hashtag = 'house'
+Follows = 0
+Likes = 0
+Comments = 0
+
 hashtagpage = 'https://www.instagram.com/explore/tags/{}'.format(hashtag)
+Follows = 201
+Likes = 0
+Comments = 0
 
 #Browser stuff
 executable_path = {'executable_path': r'C:\Users\sfbow\Desktop\chromedriver'}
@@ -32,18 +40,18 @@ browser.find_by_text("Not Now").first.click()
 time.sleep(2)
 #go to hashtag page
 browser.visit(hashtagpage)
-links = []
 
-while True:
-    browser.execute_script("window.scrollTo(0, -document.body.scrollHeight);")
-    tags = browser.links.find_by_partial_href('/p/')
-    for x in tags:
-        links.append(x['href'])
-    print(len(links))
-    """ for x in links:
-        browser.visit(x)
-        try:
-            browser.find_by_text('Follow').first.click()
-        except:
-            print('Already Followed') """
-        
+FirstThumbnail = browser.find_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div')
+FirstThumbnail.click()
+time.sleep(1)
+
+while Follows < 100:
+    if browser.is_text_present('Follow'):
+        #FollowButton = browser.find_by_tag('button')[1]
+        #FollowButton.click()
+        print('FOLLOW')
+        browser.find_link_by_text('Next').click()
+    else:
+        print('Already Followed')
+        browser.find_link_by_text('Next').click()
+    Follows += 1
